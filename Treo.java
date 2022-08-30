7b1a1c3d52d4047c069515d77d5b67803e6bf27d
Svartov <100573588+Svartov@users.noreply.github.com>
2022-08-30 21:34:12 +0200

    Update Treo.java

--- Treo.java
+++ Treo.java
@@ -1,9 +1,12 @@
     public class Treo {
+        
     public static void main(String[] args) {
 
+        // Переменные
         int num = 5;
         int numtwo;
 
+        // Выводим таблицу
         for (int i = 1; i <= 10; ++i) {
             numtwo = num * i;
             System.out.println("5 * " + i + " = " + numtwo);
