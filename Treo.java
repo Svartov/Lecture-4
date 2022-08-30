35724e307d83416dcf9a40de8a63a9e79565a9fd
Svartov <100573588+Svartov@users.noreply.github.com>
2022-08-30 22:32:32 +0300

    Add files via upload

---
+++ Treo.java
@@ -0,0 +1,13 @@
+    public class Treo {
+    public static void main(String[] args) {
+
+        int num = 5;
+        int numtwo;
+
+        for (int i = 1; i <= 10; ++i) {
+            numtwo = num * i;
+            System.out.println("5 * " + i + " = " + numtwo);
+        }
+
+    }
+}
